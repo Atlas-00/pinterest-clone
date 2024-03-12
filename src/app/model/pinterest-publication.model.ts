e04d1495d3b8edd76933bd6cb8d1d7d21d78fd2d
Naider La Fleur <naiderlafleur@gmail.com>
2024-03-12 05:07:42 -0300

    feat: Création de la structure html de la page detail-publication

--- src/app/model/pinterest-publication.model.ts
+++ src/app/model/pinterest-publication.model.ts
@@ -1,6 +1,7 @@
 export type TypePiterestPublication = {
   id: number;
   picture: string;
+  title: string;
   author: {
     name: string;
     numberFolower: number;
@@ -10,6 +11,7 @@ export type TypePiterestPublication = {
 export const PiterestPublication: TypePiterestPublication[] = [
   {
     id: 0,
+    title: 'Test0', // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/03/b2/dd/03b2dd4aa6c20f38cbf0f39ed231939c.jpg',
     author: {
@@ -19,6 +21,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 1,
+    title: 'Exploration florale', // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/37/4b/f4/374bf4073bcae8e46eb1b26baca1f6a0.jpg',
     author: {
@@ -28,6 +31,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 2,
+    title: "Coucher de soleil sur l'océan", // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/25/0a/5f/250a5f0443294b5e860a5443511e1da5.jpg',
     author: {
@@ -37,6 +41,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 3,
+    title: 'Mystère des montagnes', // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/dc/7f/56/dc7f569ff4edfda9c06ea2a60d2c0263.jpg',
     author: {
@@ -46,6 +51,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 4,
+    title: 'Forêt enchantée', // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/54/f3/f1/54f3f16e2f53699c25061fd267df0649.jpg',
     author: {
@@ -55,6 +61,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 5,
+    title: 'Jardin secret', // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/2e/be/45/2ebe45bee7ceea3807f771f28f673556.jpg',
     author: {
@@ -64,6 +71,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 6,
+    title: 'Rêverie aquatique', // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/c0/e5/84/c0e584ce40d339b97578eba6dd94b4fb.jpg',
     author: {
@@ -73,6 +81,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 7,
+    title: 'Exploration urbaine', // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/2a/4f/f6/2a4ff6b8d5a9dae6892d4cfae6d78570.jpg',
     author: {
@@ -82,6 +91,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 8,
+    title: 'Voyage en montgolfière', // Titre ajouté
     picture:
       'https://i.pinimg.com/236x/58/28/9b/58289b186e4ea85ba8b9b2ee2fcdee1e.jpg',
     author: {
@@ -89,8 +99,10 @@ export const PiterestPublication: TypePiterestPublication[] = [
       numberFolower: 999,
     },
   },
+
   {
     id: 9,
+    title: 'Titre 9',
     picture:
       'https://i.pinimg.com/236x/27/c2/d2/27c2d2f317015ae0b8f2af12d09f9905.jpg',
     author: {
@@ -100,6 +112,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 10,
+    title: 'Titre 10',
     picture:
       'https://i.pinimg.com/474x/4b/89/38/4b8938963109efa2921e5601ec2eef97.jpg',
     author: {
@@ -109,6 +122,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 11,
+    title: 'Titre 11',
     picture:
       'https://i.pinimg.com/236x/55/d0/8c/55d08cdadd8b486525703ae5866b7878.jpg',
     author: {
@@ -118,6 +132,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 12,
+    title: 'Titre 12',
     picture:
       'https://i.pinimg.com/236x/3a/de/66/3ade663097b7f67f3e06f5b0b1d0fa20.jpg',
     author: {
@@ -127,6 +142,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 13,
+    title: 'Titre 13',
     picture:
       'https://i.pinimg.com/236x/f0/f4/ff/f0f4ffa2751013566a47993c833fe999.jpg',
     author: {
@@ -136,6 +152,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 14,
+    title: 'Titre 14',
     picture:
       'https://i.pinimg.com/236x/7b/b9/23/7bb923f8dc04c8bc4b2628573565dc37.jpg',
     author: {
@@ -145,6 +162,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 15,
+    title: 'Titre 15',
     picture:
       'https://i.pinimg.com/236x/fe/c7/6f/fec76fb3cecbc1a07927542c19a005f9.jpg',
     author: {
@@ -154,6 +172,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 16,
+    title: 'Titre 16',
     picture:
       'https://i.pinimg.com/236x/87/3b/d0/873bd00d96528ad5c32d91ecebaead04.jpg',
     author: {
@@ -163,6 +182,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 17,
+    title: 'titre 17',
     picture:
       'https://i.pinimg.com/736x/4a/33/d7/4a33d7a6687c8a9293e7a449f0f0df5b.jpg',
     author: {
@@ -172,6 +192,7 @@ export const PiterestPublication: TypePiterestPublication[] = [
   },
   {
     id: 18,
+    title: 'Titre 18',
     picture:
       'https://i.pinimg.com/736x/d3/20/39/d32039d300f0f52c348f71cf052f901d.jpg',
     author: {
